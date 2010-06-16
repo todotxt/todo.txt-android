d6145bd1b5dd1eb71ab9af2d148275969773928d
Stephen Henderson <me@steveh.ca>
2010-06-15 22:18:55 -0400

    Setup Empty Unit Test Project
    * Empty unit test project added to allow for others to add to it

---
+++ tests/src/com/todotxt/todotxttouch/test/TodoTxtTouchTest.java
@@ -0,0 +1,13 @@
+package com.todotxt.todotxttouch.test;
+
+import com.todotxt.todotxttouch.TodoTxtTouch;
+import android.test.ActivityInstrumentationTestCase2;
+
+public class TodoTxtTouchTest extends
+		ActivityInstrumentationTestCase2<TodoTxtTouch> {
+
+	public TodoTxtTouchTest() {
+		super("com.todotxt.todotxttouch", TodoTxtTouch.class);
+	}
+
+}
