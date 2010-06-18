5259c2f1d8f70d15321fd515ec8b487b219a6ef7
Stephen Henderson <me@steveh.ca>
2010-06-17 21:02:21 -0400

    Unit tests * TaskHelperTest: Tests involving how file output is handled * TodoUtilTest: Tests involving how tasks are created

---
+++ tests/src/com/todotxt/todotxttouch/test/TaskHelperTest.java
@@ -0,0 +1,68 @@
+package com.todotxt.todotxttouch.test;
+
+import com.todotxt.todotxttouch.Task;
+import com.todotxt.todotxttouch.TaskHelper;
+import com.todotxt.todotxttouch.TodoUtil;
+
+import junit.framework.TestCase;
+
+public class TaskHelperTest extends TestCase {
+	
+	@Override
+	public void setUp() throws Exception {
+		super.setUp();
+	}
+	
+	
+	
+// *** START TaskHelper.toFileFormat TESTS ***
+	
+	/*
+	 * Ensure that a normal task is output the same way as it is read in.
+	 */
+	public void testToFileFormat() {
+		String expectedString = "A Simple test with no curve balls";
+		Task t = TodoUtil.createTask(0, expectedString);
+		
+		assertTrue("File output matches original input",
+				expectedString.equals(TaskHelper.toFileFormat(t)));
+	}
+	
+	/*
+	 * Ensure that a task is output to a file the same way as it was read in
+	 * with a priority.
+	 */
+	public void testToFileFormatWithPriority() {
+		String expectedString = "(A) Simple test with a priority";
+		Task t = TodoUtil.createTask(0, expectedString);
+		
+		assertTrue("File output matches original input",
+				expectedString.equals(TaskHelper.toFileFormat(t)));
+	}
+	
+	/*
+	 * Ensure that a task is output to a file the same way as it was read in
+	 * with a context.
+	 */
+	public void testToFileFormatWithContext() {
+		String expectedString = "Simple test with a context @home";
+		Task t = TodoUtil.createTask(0, expectedString);
+		
+		assertTrue("File output matches original input",
+				expectedString.equals(TaskHelper.toFileFormat(t)));
+	}
+	
+	/*
+	 * Ensure that a task is output to a file the same way as it was read in
+	 * with multiple context.
+	 */
+	public void testToFileFormatWithMultipleContexts() {
+		String expectedString = "Simple @phone test @home";
+		Task t = TodoUtil.createTask(0, expectedString);
+		
+		assertTrue("File output matches original input",
+				expectedString.equals(TaskHelper.toFileFormat(t)));
+	}
+	
+// *** END TaskHelper.toFileFormat TESTS ***
+}
--- tests/src/com/todotxt/todotxttouch/test/TodoUtilTest.java
+++ tests/src/com/todotxt/todotxttouch/test/TodoUtilTest.java
@@ -16,6 +16,8 @@ public class TodoUtilTest extends TestCase {
 		super.setUp();
 	}
 	
+// *** START TodoUtil.createTask TESTS ***
+	
 	/**
 	 * Make sure TaskUtil.createTask can properly parse a task with no priorities, contexts,
 	 * projects, due dates, etc. Vanilla task.
@@ -30,13 +32,14 @@ public class TodoUtilTest extends TestCase {
 				expectedContexts);
 		Task createdTask = TodoUtil.createTask(expectedId, expectedString);
 		
-		assertEquals(expectedTask.id, createdTask.id);
-		assertEquals(expectedTask.prio, createdTask.prio);
-		assertEquals(expectedTask.text, createdTask.text);
+		assertEquals("ID's are equal", expectedTask.id, createdTask.id);
+		assertEquals("Priorities are equal", expectedTask.prio, createdTask.prio);
+		assertTrue("Text is equal", expectedTask.text.equals(createdTask.text));
 		
 		// Should both be zero, if createdTask's contexts count is greater, something is
 		// seriously wrong.
-		assertEquals(expectedTask.contexts.size(), createdTask.contexts.size());
+		assertTrue("Items in context lists match",
+				compareTwoContextLists(expectedTask.contexts, createdTask.contexts));
 	}
 	
 	/**
@@ -53,13 +56,113 @@ public class TodoUtilTest extends TestCase {
 				expectedContexts);
 		Task createdTask = TodoUtil.createTask(expectedId, expectedString);
 		
-		assertEquals(expectedTask.id, createdTask.id);
-		assertEquals(expectedTask.prio, createdTask.prio);
-		assertEquals(expectedTask.text, createdTask.text);
+		assertEquals("ID's are equal", expectedTask.id, createdTask.id);
+		assertEquals("Priorities are equal", expectedTask.prio, createdTask.prio);
+		assertTrue("Text is equal", expectedTask.text.equals(createdTask.text));
 		
 		// Should both be zero, if createdTask's contexts count is greater, something is
 		// seriously wrong.
-		assertEquals(expectedTask.contexts.size(), createdTask.contexts.size());
+		assertTrue("Items in context lists match",
+				compareTwoContextLists(expectedTask.contexts, createdTask.contexts));
 	}
 	
+	/**
+	 * Ensure that a single context can be grabbed at the end of a string
+	 */
+	public void testCreateTaskWithContext() {
+		int expectedId = 1;
+		int expectedPriority = 0;
+		String expectedString = "Complete a simple task @phone";
+		List<String> expectedContexts = new ArrayList<String>();
+		expectedContexts.add("phone");
+		
+		Task expectedTask = new Task(expectedId, expectedPriority, expectedString,
+				expectedContexts);
+		Task createdTask = TodoUtil.createTask(expectedId, expectedString);
+		
+		assertEquals("ID's are equal", expectedTask.id, createdTask.id);
+		assertEquals("Priorities are equal", expectedTask.prio, createdTask.prio);
+		assertTrue("Text is equal", expectedTask.text.equals(createdTask.text));
+		
+		assertTrue("Items in context lists match",
+				compareTwoContextLists(expectedTask.contexts, createdTask.contexts));
+	}
+	
+	/*
+	 * Ensures that multiple contexts can be grabbed from the end of a string
+	 */
+	public void testCreateTaskWithMultipleContext() {
+		int expectedId = 1;
+		int expectedPriority = 0;
+		String expectedString = "Complete a simple task @phone @home";
+		List<String> expectedContexts = new ArrayList<String>();
+		expectedContexts.add("phone");
+		expectedContexts.add("home");
+		
+		Task expectedTask = new Task(expectedId, expectedPriority, expectedString,
+				expectedContexts);
+		Task createdTask = TodoUtil.createTask(expectedId, expectedString);
+		
+		assertEquals("ID's are equal", expectedTask.id, createdTask.id);
+		assertEquals("Priorities are equal", expectedTask.prio, createdTask.prio);
+		assertTrue("Text is equal", expectedTask.text.equals(createdTask.text));
+		
+		assertTrue("Items in context lists match",
+				compareTwoContextLists(expectedTask.contexts, createdTask.contexts));
+	}
+	
+	/*
+	 * Ensure that a context can be properly parsed out from a string if it's not at the end
+	 */
+	public void testCreateTaskWithContextInMiddle() {
+		int expectedId = 1;
+		int expectedPriority = 0;
+		String expectedString = "Complete a simple @phone task";
+		List<String> expectedContexts = new ArrayList<String>();
+		expectedContexts.add("phone");
+		
+		Task expectedTask = new Task(expectedId, expectedPriority, expectedString,
+				expectedContexts);
+		Task createdTask = TodoUtil.createTask(expectedId, expectedString);
+		
+		assertEquals("ID's are equal", expectedTask.id, createdTask.id);
+		assertEquals("Priorities are equal", expectedTask.prio, createdTask.prio);
+		assertTrue("Text is equal", expectedTask.text.equals(createdTask.text));
+		
+		assertTrue("Items in context lists match",
+				compareTwoContextLists(expectedTask.contexts, createdTask.contexts));
+	}
+	
+	public void testCreateTaskDistinguishContextFromEmail() {
+		int expectedId = 1;
+		int expectedPriority = 0;
+		String expectedString = "Email me@steveh.ca about unit testing";
+		List<String> expectedContexts = new ArrayList<String>();
+		
+		Task expectedTask = new Task(expectedId, expectedPriority, expectedString,
+				expectedContexts);
+		Task createdTask = TodoUtil.createTask(expectedId, expectedString);
+		
+		assertEquals("ID's are equal", expectedTask.id, createdTask.id);
+		assertEquals("Priorities are equal", expectedTask.prio, createdTask.prio);
+		assertTrue("Text is equal", expectedTask.text.equals(createdTask.text));
+		
+		assertTrue("Items in context lists match",
+				compareTwoContextLists(expectedTask.contexts, createdTask.contexts));
+	}
+	
+// *** START TodoUtil.createTask TESTS ***
+	
+	private boolean compareTwoContextLists(List<String> expected, List<String> created)
+	{
+		if(expected.size() != created.size())
+			return false;
+		
+		for(int i = 0; i < expected.size(); ++i) {
+			if(!expected.get(i).equals(created.get(i)))
+				return false;
+		}
+		
+		return true;
+	}
 }
