ad9104f8f88f8a6a4b1252afaeda35040e007cd8
Gina Trapani <ginatrapani@gmail.com>
2011-01-14 11:09:33 -0800

    Added license text to project source, closes #28

--- src/com/todotxt/todotxttouch/TodoTxtTouch.java
+++ src/com/todotxt/todotxttouch/TodoTxtTouch.java
@@ -1,3 +1,33 @@
+/**
+ *
+ * Todo.txt Touch/src/com/todotxt/todotxttouch/TodoTxtTouch.java
+ *
+ * Copyright (c) 2009-2011 Gina Trapani, mathias, Stephen Henderson, Tormod Haugen, shanest
+ *
+ * LICENSE:
+ *
+ * This file is part of Todo.txt Touch, an Android app for managing your todo.txt file (http://todotxt.com).
+ *
+ * Todo.txt Touch is free software: you can redistribute it and/or modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation, either version 2 of the License, or (at your option) any
+ * later version.
+ *
+ * Todo.txt Touch is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with Todo.txt Touch.  If not, see
+ * <http://www.gnu.org/licenses/>.
+ *
+ * @author Gina Trapani <ginatrapani[at]gmail[dot]com>
+ * @author mathias <mathias[at]x2[dot](none)>
+ * @author Stephen Henderson <me[at]steveh[dot]ca>
+ * @author mathias <mathias[at]ws7862[dot](none)>
+ * @author Tormod Haugen <tormodh[at]gmail[dot]com>
+ * @author shanest <ssshanest[at]gmail[dot]com>
+ * @license http://www.gnu.org/licenses/gpl.html
+ * @copyright 2009-2011 Gina Trapani, mathias, Stephen Henderson, Tormod Haugen, shanest
+ */
 package com.todotxt.todotxttouch;
 
 import java.io.IOException;
