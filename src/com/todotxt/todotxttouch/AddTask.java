5b771fa1e62a218282a6cb4ad8a16a916b80486b
Hrayr Artunyan <hrayr.artunyan@gmail.com>
2011-02-01 08:22:43 -0800

    Enabled share for other apps, it creates a new task and pasts the text in. Closes #58

--- src/com/todotxt/todotxttouch/AddTask.java
+++ src/com/todotxt/todotxttouch/AddTask.java
@@ -63,6 +63,8 @@ public class AddTask extends Activity {
 	private TodoApplication m_app;
 
 	private TextView titleBarLabel;
+	
+	private String share_text;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -81,11 +83,16 @@ public class AddTask extends Activity {
 		final Intent intent = getIntent();
 		final String action = intent.getAction();
 		// create shortcut and exit
+		// create shortcut and exit
 		if (Intent.ACTION_CREATE_SHORTCUT.equals(action)) {
 			Log.d(TAG, "Setting up shortcut icon");
 			setupShortcut();
 			finish();
 			return;
+		} else if (Intent.ACTION_SEND.equals(action)) {
+			Log.d(TAG, "Share");
+			share_text = (String)intent.getCharSequenceExtra(Intent.EXTRA_TEXT);
+			Log.d(TAG, share_text);
 		}
 
 		m_app = (TodoApplication) getApplication();
@@ -95,6 +102,11 @@ public class AddTask extends Activity {
 		// text
 		final EditText text = (EditText) findViewById(R.id.taskText);
 		text.setGravity(Gravity.TOP);
+		
+		if (share_text != null) {
+			text.setText(share_text);
+		}
+
 		Task task = (Task) getIntent().getSerializableExtra(
 				Constants.EXTRA_TASK);
 		if (task != null) {
