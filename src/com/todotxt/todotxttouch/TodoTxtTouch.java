b5b2845a7513c606f6e68924293b4ba674d63ac3
Hrayr Artunyan <hrayr.artunyan@gmail.com>
2011-02-11 10:54:30 -0800

    Adds refine & clear buttons on the actionbar for filter/search Fixes #111

--- src/com/todotxt/todotxttouch/TodoTxtTouch.java
+++ src/com/todotxt/todotxttouch/TodoTxtTouch.java
@@ -543,24 +543,7 @@ public class TodoTxtTouch extends ListActivity implements
 			startActivityForResult(settingsActivity, REQUEST_PREFERENCES);
 			break;
 		case R.id.filter:
-			Intent i = new Intent(this, Filter.class);
-
-			i.putStringArrayListExtra(Constants.EXTRA_PRIORITIES,
-					TaskHelper.getPrios(m_tasks));
-			i.putStringArrayListExtra(Constants.EXTRA_PROJECTS,
-					TaskHelper.getProjects(m_tasks));
-			i.putStringArrayListExtra(Constants.EXTRA_CONTEXTS,
-					TaskHelper.getContexts(m_tasks));
-
-			i.putStringArrayListExtra(Constants.EXTRA_PRIORITIES_SELECTED,
-					m_prios);
-			i.putStringArrayListExtra(Constants.EXTRA_PROJECTS_SELECTED,
-					m_projects);
-			i.putStringArrayListExtra(Constants.EXTRA_CONTEXTS_SELECTED,
-					m_contexts);
-			i.putExtra(Constants.EXTRA_SEARCH, m_search);
-
-			startActivityIfNeeded(i, REQUEST_FILTER);
+			startFilterActivity();
 			break;
 		case R.id.sort:
 			AlertDialog.Builder builder = new AlertDialog.Builder(this);
@@ -681,7 +664,24 @@ public class TodoTxtTouch extends ListActivity implements
 		updateRefreshStatus();
 		populateFromExternal();
 	}
-
+	
+	/** Handle refine filter click **/
+	public void onRefineClick(View v) {
+		startFilterActivity();
+	}
+	
+	/** Handle clear filter click **/
+	public void onClearClick(View v) {
+		/*TODO not the best way to do this */
+		clearFilter();
+		if (m_search != ""){
+			setFilteredTasks(false);
+			finish();
+		} else {
+			setFilteredTasks(false);
+		}
+	}
+	
 	void clearFilter() {
 		m_prios = new ArrayList<String>(); // Collections.emptyList();
 		m_contexts = new ArrayList<String>(); // Collections.emptyList();
@@ -723,8 +723,8 @@ public class TodoTxtTouch extends ListActivity implements
 		}
 
 		final TextView filterText = (TextView) findViewById(R.id.filter_text);
-		final LinearLayout subtitle_bar = (LinearLayout) findViewById(R.id.subtitle_bar);
-		final ImageView subtitle_icon = (ImageView) findViewById(R.id.subtitle_icon);
+		final LinearLayout actionbar = (LinearLayout) findViewById(R.id.actionbar);
+		final ImageView actionbar_icon = (ImageView) findViewById(R.id.actionbar_icon);
 
 		if (filterText != null) {
 			if (m_filters.size() > 0) {
@@ -737,24 +737,24 @@ public class TodoTxtTouch extends ListActivity implements
 				if (!Strings.isEmptyOrNull(m_search)) {
 					filterTitle += "Keyword";
 				}
-				subtitle_icon.setImageResource(R.drawable.ic_subtitle_filter);
+				actionbar_icon.setImageResource(R.drawable.ic_actionbar_filter);
 
-				subtitle_bar.setVisibility(View.VISIBLE);
+				actionbar.setVisibility(View.VISIBLE);
 				filterText.setText(filterTitle);
 
 			} else if (!Strings.isEmptyOrNull(m_search)) {
 				if (filterText != null) {
 
-					subtitle_icon
-							.setImageResource(R.drawable.ic_subtitle_search);
+					actionbar_icon
+							.setImageResource(R.drawable.ic_actionbar_search);
 					filterText.setText(getString(R.string.title_search_results)
 							+ " " + m_search);
 
-					subtitle_bar.setVisibility(View.VISIBLE);
+					actionbar.setVisibility(View.VISIBLE);
 				}
 			} else {
 				filterText.setText("");
-				subtitle_bar.setVisibility(View.GONE);
+				actionbar.setVisibility(View.GONE);
 			}
 		}
 	}
@@ -895,5 +895,25 @@ public class TodoTxtTouch extends ListActivity implements
 	public void showToast(String string) {
 		Util.showToastLong(this, string);
 	}
+	
+	public void startFilterActivity() {
+		Intent i = new Intent(this, Filter.class);
 
+		i.putStringArrayListExtra(Constants.EXTRA_PRIORITIES,
+				TaskHelper.getPrios(m_tasks));
+		i.putStringArrayListExtra(Constants.EXTRA_PROJECTS,
+				TaskHelper.getProjects(m_tasks));
+		i.putStringArrayListExtra(Constants.EXTRA_CONTEXTS,
+				TaskHelper.getContexts(m_tasks));
+
+		i.putStringArrayListExtra(Constants.EXTRA_PRIORITIES_SELECTED,
+				m_prios);
+		i.putStringArrayListExtra(Constants.EXTRA_PROJECTS_SELECTED,
+				m_projects);
+		i.putStringArrayListExtra(Constants.EXTRA_CONTEXTS_SELECTED,
+				m_contexts);
+		i.putExtra(Constants.EXTRA_SEARCH, m_search);
+
+		startActivityIfNeeded(i, REQUEST_FILTER);
+	}
 }
