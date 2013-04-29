dc13bfe2c909c140b6b797a655bc5ab1e0d2c13d
Mark Janssen <mpc.janssen@gmail.com>
2013-04-29 15:30:11 +0200

    Create multiple tasks with newline

--- src/com/todotxt/todotxttouch/AddTask.java
+++ src/com/todotxt/todotxttouch/AddTask.java
@@ -47,7 +47,7 @@ import com.todotxt.todotxttouch.task.TaskBag;
 import com.todotxt.todotxttouch.util.Util;
 
 public class AddTask extends SherlockActivity {
-
+			
 	private final static String TAG = AddTask.class.getSimpleName();
 
 	private ProgressDialog m_ProgressDialog = null;
@@ -72,8 +72,7 @@ public class AddTask extends SherlockActivity {
 	public boolean onMenuItemSelected(int featureId, MenuItem item) {
 		switch (item.getItemId()) {
 		case R.id.menu_save_task:
-			final String input = textInputField.getText().toString()
-					.replaceAll("\\r\\n|\\r|\\n", " ");
+			final String input = textInputField.getText().toString();
 			addEditAsync(input);
 			break;
 		case R.id.menu_add_task_help:
@@ -159,27 +158,17 @@ public class AddTask extends SherlockActivity {
 
 	private void showProjectContextMenu() {
 		AlertDialog.Builder builder = new AlertDialog.Builder(this);
-		final ArrayList<String> allProjectsContexts = new ArrayList<String>();
-		ArrayList<String> contexts = taskBag.getContexts(false);
-		Collections.sort(contexts);
-		for (String item : contexts) {
-			allProjectsContexts.add("@" + item);
-		}
-
-		ArrayList<String> projects = taskBag.getProjects(false);
-		Collections.sort(projects);
-		for (String item : projects) {
-			allProjectsContexts.add("+" + item);
-		}
-		if (allProjectsContexts.size() == 0) {
+		final ArrayList<String> labels = new ArrayList<String>();
+		labels.addAll(labelsInTaskbagAndText());
+		if (labels.size() == 0) {
 			Util.showToastLong(this, R.string.nocontextsprojectsadd);
 			return;
 		}
-		builder.setItems(allProjectsContexts.toArray(new String[0]),
+		builder.setItems(labels.toArray(new String[0]),
 				new OnClickListener() {
 					@Override
 					public void onClick(DialogInterface arg0, int which) {
-						replaceTextAtSelection(allProjectsContexts.get(which)
+						replaceTextAtSelection(labels.get(which)
 								+ " ");
 					}
 				});
@@ -256,10 +245,13 @@ public class AddTask extends SherlockActivity {
 					Task task = (Task) params[0];
 					String input = (String) params[1];
 					if (task != null) {
+						input = input.replaceAll("\\r\\n|\\r|\\n", " ");
 						task.update(input);
 						taskBag.update(task);
 					} else {
-						taskBag.addAsTask(input);
+						for (String text : input.split("\\r\\n|\\r|\\n")) {
+							taskBag.addAsTask(text);
+						}
 					}
 
 					// make widgets update
@@ -316,5 +308,29 @@ public class AddTask extends SherlockActivity {
 	public void onHelpClick() {
 		Intent intent = new Intent(getApplicationContext(), HelpActivity.class);
 		startActivity(intent);
+	}
+	
+	private ArrayList<String> labelsInTaskbagAndText() {
+		/* Returns the defined labels in the taskbag
+		 * and the current task being added.
+		 * This way when adding multiple tasks labels from 
+		 * previous lines will be available fordropdown
+		 */		
+		ArrayList<String>labels = new ArrayList<String>();		
+		Task temp = new Task(1,textInputField.getText().toString());
+		
+		ArrayList<String> contexts = taskBag.getContexts(false);
+		contexts.addAll(temp.getContexts());
+		Collections.sort(contexts);
+		for (String item : contexts) {
+			labels.add("@" + item);
+		}
+		ArrayList<String> projects = taskBag.getProjects(false);
+		projects.addAll(temp.getProjects());
+		Collections.sort(projects);
+		for (String item : projects) {
+			labels.add("+" + item);
+		}
+		return labels;				
 	}
 }
